6c7aea73212369048bbd01d285a90ee486065ae0
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-28 01:55:10 +0900

    a

--- sand-box/js/script.js
+++ sand-box/js/script.js
@@ -17,23 +17,46 @@ $(function(){
 /* section 1 Fortune */
 
 document.addEventListener('DOMContentLoaded', () => {
-    const section01Target = document.getElementById('section01-target');
     const section01Toggle = document.getElementById('section01-toggle');
-    section01Target.addEventListener('click', () => {
-        section01Target.classList.add('section01-circle');
-    });
     section01Toggle.addEventListener('click', () => {
         section01Toggle.classList.toggle('section01-circle');
     });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const fortune = document.createElement('div');
-    fortune.classList.add('section01-box');
-    fortune.addEventListener('click', () => {
-        fortune.classList.toggle('section01-circle');
-    });
-
-    const fortuneWrapper = document.getElementById('section01-fortune-wrapper');
-    fortuneWrapper.appendChild(fortune);
+    const num = 5;
+    const winner = Math.floor(Math.random() * num);
+    for(let i=0; i<num; i++){
+        const fortune = document.createElement('div');
+        fortune.classList.add('section01-box');
+        fortune.addEventListener('click', () => {
+            if ( i == winner ) {
+                fortune.classList.add('section01-win');
+                fortune.textContent = 'WIN';
+            } else {
+                fortune.classList.add('section01-lose');
+                fortune.textContent = 'LOSE';
+            }
+        });
+        const fortuneWrapper = document.getElementById('section01-fortune-wrapper');
+        fortuneWrapper.appendChild(fortune);        
+    }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('section01-btn');
+    btn.addEventListener('click',() => {
+        const n = Math.floor(Math.random() * 4);
+        switch(n){
+            case 0:
+            btn.textContent = '大吉';
+            break；
+            switch(n){
+
+
+
+
+
+
+
+                
